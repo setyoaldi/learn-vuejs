f06fa3d8842adecbcbf8bda160a2a3d720ae8743
setyoaldi <achmadsetyoaldi@gmail.com>
2023-10-05 14:42:54 +0700

    feat : add reset count button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+var count = ref(0);
 </script>
 
 <template>
@@ -11,6 +11,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count = 0">Reset Count</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
